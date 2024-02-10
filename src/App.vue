78c7dcbf0ab2c2ec37db0fd9fab6e486d9853ee7
NataliyaNikulshina <victory515@yandex.ru>
2024-02-10 19:51:15 +0300

    feat: пагинация, постраничный вывод постов

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,20 @@
     </MyModal>
     <PostList :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isLoading" />
     <div v-else>Загрузка ...</div>
+    <NumberPages :pages="totalPages" :pagesCurrent="page" @update:pagesCurrent="setCurrentPage" />
   </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import NumberPages from "@/components/NumberPages.vue";
 import axios from "axios";
 export default {
   components: {
     PostForm,
     PostList,
+    NumberPages
 },
   data() {
     return {
@@ -32,6 +35,9 @@ export default {
       isLoading: false,
       selectedSort: "",
       searchQuery: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         { value: "title", name: "По названию" },
         { value: "body", name: "По содержанию" },
@@ -51,12 +57,21 @@ export default {
     showModal(modalVisible) {
       this.modalVisible = !modalVisible;
     },
+    setCurrentPage(pageNumber) {
+      this.page = pageNumber;
+    },
     async feachPosts() {
       try {
         this.isLoading = true;
         const responce = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          "https://jsonplaceholder.typicode.com/posts", {
+            params: {
+              _page: this.page,
+              _limit: this.limit
+            }
+          }
         );
+        this.totalPages = Math.ceil(responce.headers['x-total-count'] / this.limit);
         this.posts = responce.data;
         // console.log(responce);
       } catch (e) {
@@ -81,25 +96,29 @@ export default {
       return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
     }
   },
-  // watch: {
-  //   selectedSort(newValue) {
-  //     if (newValue === "id") {
-  //       this.posts.sort((a, b) => {
-  //         return a.id - b.id;
-  //       });
-  //     } else {
-  //       console.log(newValue);
-  //       this.posts.sort((post1, post2) => {
-  //         return post1[newValue]?.localeCompare(post2[newValue]);
-  //       });
-  //     }
-  //   },
-  // },
+  watch: {
+    page() {
+      this.feachPosts()
+    },
+    // selectedSort(newValue) {
+    //   if (newValue === "id") {
+    //     this.posts.sort((a, b) => {
+    //       return a.id - b.id;
+    //     });
+    //   } else {
+    //     console.log(newValue);
+    //     this.posts.sort((post1, post2) => {
+    //       return post1[newValue]?.localeCompare(post2[newValue]);
+    //     });
+    //   }
+    // },
+  },
 };
 </script>
 
 <style>
 * {
+  font-family: Arial, Helvetica, sans-serif;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="post">
     <div>
-        <div><strong>Название: </strong>{{ post.title }}</div>
-        <div><strong>Описание: </strong>{{ post.body }}</div>
+      <div>№ {{ post.id }}</div>
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
     </div>
     <div class="post__btns">
         <MyButton @click="$emit('remove', post)">Удалить</MyButton>
